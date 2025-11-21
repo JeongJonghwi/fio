403ae7cb6b2d9b7095209aa022c61ad5c1190b75
Jonghwi Jeong <jongh2.jeong@samsung.com>
2025-11-21 13:05:28 +0900

    multi_range_trim -> multi_range_cmd
    Signed-off-by: Jonghwi Jeong <jongh2.jeong@samsung.com>

--- fio.h
+++ fio.h
@@ -638,9 +638,9 @@ static inline void fio_ro_check(const struct thread_data *td, struct io_u *io_u)
 	 */
 }
 
-static inline bool multi_range_trim(struct thread_data *td, struct io_u *io_u)
+static inline bool multi_range_cmd(struct thread_data *td, struct io_u *io_u)
 {
-	if (io_u->ddir == DDIR_TRIM && td->o.num_range > 1)
+	if (td->o.num_range > 1)
 		return true;
 
 	return false;
